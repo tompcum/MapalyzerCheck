70d5b216e99a3266f47dc1cb6f39c64de11f8b3b
PetjaPaivarinta <Petja.Paivarinta@proton.me>
2025-01-13 14:10:38 +0200

    added functioning gpx

--- app/src/renderer/src/App.tsx
+++ app/src/renderer/src/App.tsx
@@ -1,19 +1,31 @@
 import L from 'leaflet'
 import 'leaflet/dist/leaflet.css';
+import 'leaflet-gpx'
 
 import { useEffect } from 'react'
 
 function App(): JSX.Element {
 
+
   useEffect(() => {
-    var map = L.map('map').setView([51.505, -0.09], 13);
+    var map = L.map('map');
     
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 19,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
   }).addTo(map);
 
-  map.invalidateSize()
+  const url = 'https://mpetazzoni.github.io/leaflet-gpx/demo.gpx';  // URL to your GPX file
+  const options = {
+    async: true,
+    polyline_options: { color: 'red' },
+  };
+
+  console.log(options); // Check the options object
+  const gpx = new L.GPX(url, options).on('loaded', (e) => {
+    map.fitBounds(e.target.getBounds());
+  }).addTo(map);
+
   })
 
 
